f8d9e4b82a3255a5841c76873d7831682808d1f0
arturlazoryk95 <126485696+arturlazoryk95@users.noreply.github.com>
2025-02-07 23:36:27 +0100

    updated main section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Start now baby!</button>
+      <h1>Collect customer feedback please!</h1>
+      <div>Create super nice dashboard!</div>
     </main>
   );
 }
