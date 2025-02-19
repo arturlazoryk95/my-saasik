15b3a14734379f5a98ca9ec904ad26f206be56ae
arturlazoryk95 <126485696+arturlazoryk95@users.noreply.github.com>
2025-02-19 12:16:08 +0000

    wow pushed the fillButtonforLagrima :)

--- components/FormFakturka.js
+++ components/FormFakturka.js
@@ -5,15 +5,34 @@ import { useState } from "react";
 
 import pdfMake, { vfs } from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+import { endOfMonth, subMonths, getISOWeek, format } from "date-fns";
 pdfMake.addVirtualFileSystem(pdfFonts);
 
 const FormFakturka = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    generateInvoicePDF3();
+    console.log("Robimy to ...");
+    generateInvoicePDF();
     toast.success("Udało się!");
   };
 
+  const handleFillLagrima = (event) => {
+    const weekNumber = getISOWeek(new Date());
+    const lastMonthDate = endOfMonth(subMonths(new Date(), 1));
+    console.log("Wypełniamy ...");
+    event.preventDefault();
+    setNip("8381741305");
+    setNazwaKontrahenta("Lagrima Sp. z o. o.");
+    setNipFirmy("9671438426");
+    setNazwaUslugi(
+      "Software Management - HubSpot, websites, internal processes"
+    );
+    setNumerFaktury(`2025/${weekNumber}/1`);
+    setDataFaktury(format(lastMonthDate, "yyyy-MM-dd"));
+    setKwotaNetto(4000);
+    setStawkaVat(0.23);
+    setNazwaFirmy("Artur Lazoryk Business Advisory");
+  };
   // Declare state for all the form fields
   const [nip_kontrahenta, setNip] = useState("");
   const [nazwa_kontrahenta, setNazwaKontrahenta] = useState("");
@@ -30,8 +49,14 @@ const FormFakturka = () => {
     setter(event.target.value);
   };
 
-  const generateInvoicePDF3 = () => {
+  const generateInvoicePDF = () => {
     const docDefinition = {
+      // header: [
+      //   {
+      //     image:
+      //       "http://www.hessionphysicaltherapy.ie/wp-content/uploads/2010/11/dummy-logo1.jpg",
+      //   },
+      // ],
       content: [
         { text: "Faktura VAT", style: "header" },
 
@@ -104,6 +129,7 @@ const FormFakturka = () => {
         },
       },
     };
+    console.log("Generating PDF...");
 
     pdfMake
       .createPdf(docDefinition)
@@ -111,172 +137,177 @@ const FormFakturka = () => {
   };
 
   return (
-    <form
-      className="bg-base-300 p-8 rounded-3xl space-y-8 max-w-2xl mx-auto"
-      onSubmit={handleSubmit}
-    >
-      <div className="flex justify-center gap-7">
-        <div className="space-y-4">
-          {/* Mój NIP */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Mój NIP</span>
-            </div>
-            <input
-              required
-              type="text"
-              placeholder="967-92-38-136"
-              className="input input-bordered w-full max-w-xs"
-              value={nip_firmy}
-              onChange={(event) => handleChange(event, setNipFirmy)}
-            />
-          </label>
+    <div className="flex gap-3">
+      <form
+        className="bg-base-300 p-8 rounded-3xl space-y-8 max-w-2xl mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex justify-center gap-7">
+          <div className="space-y-4">
+            {/* Mój NIP */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Mój NIP</span>
+              </div>
+              <input
+                required
+                type="text"
+                placeholder="967-92-38-136"
+                className="input input-bordered w-full max-w-xs"
+                value={nip_firmy}
+                onChange={(event) => handleChange(event, setNipFirmy)}
+              />
+            </label>
 
-          {/* Nazwa firmy */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Nazwa firmy</span>
-            </div>
-            <input
-              required
-              type="text"
-              placeholder="Moja Firma Sp. z o.o."
-              className="input input-bordered w-full max-w-xs"
-              value={nazwa_firmy}
-              onChange={(event) => handleChange(event, setNazwaFirmy)}
-            />
-          </label>
+            {/* Nazwa firmy */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Nazwa firmy</span>
+              </div>
+              <input
+                required
+                type="text"
+                placeholder="Moja Firma Sp. z o.o."
+                className="input input-bordered w-full max-w-xs"
+                value={nazwa_firmy}
+                onChange={(event) => handleChange(event, setNazwaFirmy)}
+              />
+            </label>
 
-          {/* NIP kontrahenta */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">NIP kontrahenta</span>
-            </div>
-            <input
-              required
-              type="text"
-              placeholder="967-92-38-136"
-              className="input input-bordered w-full max-w-xs"
-              value={nip_kontrahenta}
-              onChange={(event) => handleChange(event, setNip)}
-            />
-          </label>
+            {/* NIP kontrahenta */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">NIP kontrahenta</span>
+              </div>
+              <input
+                required
+                type="text"
+                placeholder="967-92-38-136"
+                className="input input-bordered w-full max-w-xs"
+                value={nip_kontrahenta}
+                onChange={(event) => handleChange(event, setNip)}
+              />
+            </label>
 
-          {/* Nazwa kontrahenta */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Nazwa kontrahenta</span>
-            </div>
-            <input
-              required
-              type="text"
-              placeholder="Kowalski Big Biznes Sp. z o. o."
-              className="input input-bordered w-full max-w-xs"
-              value={nazwa_kontrahenta}
-              onChange={(event) => handleChange(event, setNazwaKontrahenta)}
-            />
-          </label>
+            {/* Nazwa kontrahenta */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Nazwa kontrahenta</span>
+              </div>
+              <input
+                required
+                type="text"
+                placeholder="Kowalski Big Biznes Sp. z o. o."
+                className="input input-bordered w-full max-w-xs"
+                value={nazwa_kontrahenta}
+                onChange={(event) => handleChange(event, setNazwaKontrahenta)}
+              />
+            </label>
 
-          {/* Nazwa usługi */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Nazwa usługi</span>
-            </div>
-            <input
-              required
-              type="text"
-              placeholder="Sprzedaż oprogramowania"
-              className="input input-bordered w-full max-w-xs"
-              value={nazwa_uslugi}
-              onChange={(event) => handleChange(event, setNazwaUslugi)}
-            />
-          </label>
+            {/* Nazwa usługi */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Nazwa usługi</span>
+              </div>
+              <input
+                required
+                type="text"
+                placeholder="Sprzedaż oprogramowania"
+                className="input input-bordered w-full max-w-xs"
+                value={nazwa_uslugi}
+                onChange={(event) => handleChange(event, setNazwaUslugi)}
+              />
+            </label>
+          </div>
+
+          <div className="space-y-4">
+            {/* Numer faktury */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Numer faktury</span>
+              </div>
+              <input
+                required
+                type="text"
+                placeholder="2025/5/1"
+                className="input input-bordered w-full max-w-xs"
+                value={numer_faktury}
+                onChange={(event) => handleChange(event, setNumerFaktury)}
+              />
+            </label>
+            {/* Kwota netto */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Kwota netto</span>
+              </div>
+              <input
+                required
+                type="number"
+                placeholder="1000.50"
+                step="0.01"
+                min="0"
+                className="input input-bordered w-full max-w-xs"
+                value={kwota_netto}
+                onChange={(event) => handleChange(event, setKwotaNetto)}
+              />
+            </label>
+
+            {/* Stawka VAT */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Stawka % VAT</span>
+              </div>
+              <input
+                required
+                type="number"
+                placeholder="0.23"
+                step="0.01"
+                min="0"
+                max="1"
+                className="input input-bordered w-full max-w-xs"
+                value={stawka_vat}
+                onChange={(event) => handleChange(event, setStawkaVat)}
+              />
+            </label>
+
+            {/* Data na fakturze */}
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Data na fakturze</span>
+              </div>
+              <input
+                required
+                type="date"
+                className="input input-bordered w-full max-w-xs"
+                value={data_faktury}
+                onChange={(event) => handleChange(event, setDataFaktury)}
+              />
+            </label>
+          </div>
         </div>
 
-        <div className="space-y-4">
-          {/* Numer faktury */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Numer faktury</span>
-            </div>
-            <input
-              required
-              type="text"
-              placeholder="2025/5/1"
-              className="input input-bordered w-full max-w-xs"
-              value={numer_faktury}
-              onChange={(event) => handleChange(event, setNumerFaktury)}
+        <button className="btn btn-primary mt-6 w-full" type="submit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
             />
-          </label>
-          {/* Kwota netto */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Kwota netto</span>
-            </div>
-            <input
-              required
-              type="number"
-              placeholder="1000.50"
-              step="0.01"
-              min="0"
-              className="input input-bordered w-full max-w-xs"
-              value={kwota_netto}
-              onChange={(event) => handleChange(event, setKwotaNetto)}
-            />
-          </label>
-
-          {/* Stawka VAT */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Stawka % VAT</span>
-            </div>
-            <input
-              required
-              type="number"
-              placeholder="0.23"
-              step="0.01"
-              min="0"
-              max="1"
-              className="input input-bordered w-full max-w-xs"
-              value={stawka_vat}
-              onChange={(event) => handleChange(event, setStawkaVat)}
-            />
-          </label>
-
-          {/* Data na fakturze */}
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Data na fakturze</span>
-            </div>
-            <input
-              required
-              type="date"
-              className="input input-bordered w-full max-w-xs"
-              value={data_faktury}
-              onChange={(event) => handleChange(event, setDataFaktury)}
-            />
-          </label>
-        </div>
-      </div>
-
-      <button className="btn btn-primary mt-6 w-full" type="submit">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-          />
-        </svg>
-        Pobierz PDF
+          </svg>
+          Pobierz PDF
+        </button>
+      </form>
+      <button onClick={handleFillLagrima} className="btn btn-secondary">
+        Fill Lagrima
       </button>
-    </form>
+    </div>
   );
 };
 
